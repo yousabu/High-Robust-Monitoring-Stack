f3e7ea426c02f358a59d5767c8545434ae976012
Youssef Abuhamda <66924041+yousabu@users.noreply.github.com>
2024-08-26 15:28:38 +0300

    Create installation.sh

---
+++ Logstash/installation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elastic-keyring.gpg
+sudo apt-get install apt-transport-https
+echo "deb [signed-by=/usr/share/keyrings/elastic-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-8.x.list
+
+sudo apt-get update && sudo apt-get install logstash
